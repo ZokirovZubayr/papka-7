15b832db276413803f039986028ecaa84e537d35
ZokirovZubayr <zokirovzubayr@gmail.com>
2024-02-26 22:07:10 -0800

    I want to learn git and github so I add this commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+let age = prompt( ' age ')
+alert(age )
+
+let fam = promt( ' last name ')
+alert(fam)
+
+let name = prompt( ' name' )
+alert(name)
+
+aler('age = ' + age + 'mening ismim = ' + name + "")
